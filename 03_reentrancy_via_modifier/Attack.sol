4681a781dc13d5b2930be9a8d71c83de9d812097
Phuwanai Thummavet <mr.thummavet@gmail.com>
2022-04-20 10:38:00 +0700

    add links to medium (Reentrancy via Modifier)

--- 03_reentrancy_via_modifier/Attack.sol
+++ 03_reentrancy_via_modifier/Attack.sol
@@ -5,7 +5,7 @@
 //--------------------------------------------------------------------------//
 
 // For more info, please refer to my article:
-//  - On Medium: (coming soon)
+//  - On Medium: https://medium.com/valixconsulting/solidity-smart-contract-security-by-example-03-reentrancy-via-modifier-fba6b1d8ff81
 //  - On serial-coder.com: (coming soon)
 
 pragma solidity 0.8.13;
