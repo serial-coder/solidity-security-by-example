0cc4269391a4a20238e2fb5a9b45ee30708a05b6
Phuwanai Thummavet <mr.thummavet@gmail.com>
2022-05-09 18:28:13 +0700

    add links to medium (Cross-Function Reentrancy)

--- 04_cross_function_reentrancy/Attack.sol
+++ 04_cross_function_reentrancy/Attack.sol
@@ -5,7 +5,7 @@
 //--------------------------------------------------------------------------//
 
 // For more info, please refer to my article:
-//  - On Medium: (coming soon)
+//  - On Medium: https://medium.com/valixconsulting/solidity-smart-contract-security-by-example-04-cross-function-reentrancy-de9cbce0558e
 //  - On serial-coder.com: (coming soon)
 
 pragma solidity 0.8.13;
